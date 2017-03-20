458936a4d5149fbedab653420ef3dcc844d93e78
ryonext <ryonext.s@gmail.com>
2017-03-20 18:13:02 +0900

    Separate script to other files.

---
+++ post_build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git clone https://github.com/ryonext/rails_update_by_codebuild.git
+cp Gemfile.lock ./rails_update_by_codebuild/Gemfile.lock
+cd ./rails_update_by_codebuild
+git checkout -b bundle_update_`date "+%s"`
+git add Gemfile.lock
+git commit -m "Today's bundle update"
+git push
+# create PR
