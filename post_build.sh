63e70e6bf23e54e91034a63f70e072e9e3a83241
ryonext <ryonext.s@gmail.com>
2017-03-20 19:09:14 +0900

    Check git

--- post_build.sh
+++ post_build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-
-aws s3 cp $S3_ID_RSA ~/.ssh/id_rsa
-cp ssh_config ~/.ssh/config
-
-git clone git@github.com:ryonext/rails_update_by_codebuild.git
-cp Gemfile.lock ./rails_update_by_codebuild/Gemfile.lock
-cd ./rails_update_by_codebuild
-git checkout -b bundle_update_`date "+%s"`
-git add Gemfile.lock
-git commit -m "Today's bundle update"
-git push
-# create PR
+git status
+#
+#aws s3 cp $S3_ID_RSA ~/.ssh/id_rsa
+#cp ssh_config ~/.ssh/config
+#
+#git clone git@github.com:ryonext/rails_update_by_codebuild.git
+#cp Gemfile.lock ./rails_update_by_codebuild/Gemfile.lock
+#cd ./rails_update_by_codebuild
+#git checkout -b bundle_update_`date "+%s"`
+#git add Gemfile.lock
+#git commit -m "Today's bundle update"
+#git push
+## create PR
