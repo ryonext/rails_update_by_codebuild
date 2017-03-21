c31ec67dfb4ffaa9a3390cf0ff784aba870c2bd0
ryonext <ryonext.s@gmail.com>
2017-03-21 11:51:21 +0900

    Try to change script

--- post_build.sh
+++ post_build.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-git status
-#
 #aws s3 cp $S3_ID_RSA ~/.ssh/id_rsa
 #cp ssh_config ~/.ssh/config
-#
 #git clone git@github.com:ryonext/rails_update_by_codebuild.git
 #cp Gemfile.lock ./rails_update_by_codebuild/Gemfile.lock
 #cd ./rails_update_by_codebuild
-#git checkout -b bundle_update_`date "+%s"`
-#git add Gemfile.lock
-#git commit -m "Today's bundle update"
-#git push
-## create PR
+git checkout -b bundle_update_`date "+%s"`
+git add Gemfile.lock
+git commit -m "Today's bundle update"
+git push origin
+# create PR
